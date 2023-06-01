dea841c2a5cbc010aa52eb984e7bd60edc86833c
danimili <danielamilieris@gmail.com>
2023-06-01 18:17:33 +0300

    redux

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,129 @@
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import axios from "axios";
+import {
+  setCurrentPage,
+  setSelectedType,
+  setIsLoading,
+  setIsTypeModalOpen,
+  setIsPictureModalOpen,
+  setSelectedPicture,
+  fetchPhotos,
+} from "../store/store";
+
+import TypeModal from "../modal/typeModal";
+import PictureModal from "../modal/pictureModal";
+
+const AppContainer = () => {
+  const dispatch = useDispatch();
+
+  const photos = useSelector((state) => state.photos.photos);
+  const currentPage = useSelector((state) => state.photos.currentPage);
+  const selectedType = useSelector((state) => state.photos.selectedType);
+  const isLoading = useSelector((state) => state.photos.isLoading);
+  const isTypeModalOpen = useSelector((state) => state.photos.isTypeModalOpen);
+  const isPictureModalOpen = useSelector((state) => state.photos.isPictureModalOpen);
+  const selectedPicture = useSelector((state) => state.photos.selectedPicture);
+
+  const handleTypeModalOpen = () => {
+    dispatch(setIsTypeModalOpen(true));
+  };
+
+  const handleTypeModalClose = () => {
+    dispatch(setIsTypeModalOpen(false));
+  };
+
+  const handleTypeSelect = async ({ category }) => {
+    dispatch(setSelectedType(category));
+    dispatch(setIsTypeModalOpen(false));
+
+    try {
+      const response = await axios.get(`/api/photos?category=${category}&page=${currentPage}`);
+      dispatch(fetchPhotos(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePictureClick = (photo) => {
+    dispatch(setSelectedPicture(photo));
+    dispatch(setIsPictureModalOpen(true));
+    dispatch(setSelectedType(null));
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      dispatch(setCurrentPage(currentPage - 1));
+    }
+  };
+
+  const handleNextClick = () => {
+    dispatch(setCurrentPage(currentPage + 1));
+    dispatch(fetchPhotos({ category: selectedType, page: currentPage + 1 }));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(setIsLoading(true));
+      try {
+        const response = await axios.get(`/api/photos?type=${selectedType}&page=${currentPage}`);
+        dispatch(fetchPhotos(response.data));
+      } catch (error) {
+        console.log(error);
+      } finally {
+        dispatch(setIsLoading(false));
+      }
+    };
+    fetchData();
+  }, [currentPage, selectedType, dispatch]);
+
+  return (
+    <>
+      <body>
+        <div>
+          {/* Pagination buttons */}
+          <div className='buttons'>
+            <button className="prev-button" onClick={handlePrevClick} disabled={currentPage === 1}>
+              &#8249; Prev
+            </button>
+            <button className="type-button" onClick={handleTypeModalOpen}>
+              Select Type
+            </button>
+            <button className="next-button" onClick={handleNextClick}>
+              Next &#8250;
+            </button>
+          </div>
+
+          {/* Render the fetched photos */}
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <div className="photo-grid">
+              {photos.map((photo) => (
+                <img
+                  src={photo.largeImageURL}
+                  alt={photo.tags}
+                  className="photo"
+                  key={photo.id}
+                  onClick={() => handlePictureClick(photo)}
+                />
+              ))}
+            </div>
+          )}
+
+          {/* Render the type modal */}
+          {isTypeModalOpen && (
+            <TypeModal isOpen={isTypeModalOpen} onClose={handleTypeModalClose} onTypeSelect={handleTypeSelect} />
+          )}
+
+          {/* Render the picture modal */}
+          {selectedPicture && isPictureModalOpen && (
+            <PictureModal isOpen={isPictureModalOpen} onClose={() => dispatch(setIsPictureModalOpen(false))} picture={selectedPicture} />
+          )}
+        </div>
+      </body>
+    </>
+  );
+};
+
+export default AppContainer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,82 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/reducer';
+import { configureStore, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const store = createStore(rootReducer);
+// Defining the initial state
+const initialState = {
+  photos: [],
+  error: null,
+  currentPage: 1,
+  selectedType: "",
+  isLoading: false,
+  isTypeModalOpen: false,
+  isPictureModalOpen: false,
+  selectedPicture: null,
+};
+
+// Create an async thunk to fetch the photos
+export const fetchPhotos = createAsyncThunk(
+  "photos/fetchPhotos",
+  async ({ category, page }) => {
+    try {
+      const response = await axios.get(`/api/photos?category=${category}&page=${page}`);
+      // Handle the received images data
+      return response.data;
+    } catch (error) {
+      // Handle the error
+      throw new Error(error.message);
+    }
+  }
+);
+
+// Create a slice for the photos
+const photosSlice = createSlice({
+  name: "photos",
+  initialState,
+  reducers: {
+    setCurrentPage: (state, action) => {
+      state.currentPage = action.payload;
+    },
+    setSelectedType: (state, action) => {
+      state.selectedType = action.payload;
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setIsTypeModalOpen: (state, action) => {
+      state.isTypeModalOpen = action.payload;
+    },
+    setIsPictureModalOpen: (state, action) => {
+      state.isPictureModalOpen = action.payload;
+    },
+    setSelectedPicture: (state, action) => {
+      state.selectedPicture = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPhotos.fulfilled, (state, action) => {
+      state.photos = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchPhotos.rejected, (state, action) => {
+      state.error = action.error.message;
+    });
+  },
+});
+
+// Create the Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  reducer: {
+    photos: photosSlice.reducer,
+  },
+});
+
+export const {
+  setCurrentPage,
+  setSelectedType,
+  setIsLoading,
+  setIsTypeModalOpen,
+  setIsPictureModalOpen,
+  setSelectedPicture,
+} = photosSlice.actions;
 
 export default store;
